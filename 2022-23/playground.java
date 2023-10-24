80ee05b1fd2c5681dd3d2ec276f7221dd865248d
AsherChilli <asherchildress@gmail.com>
2023-10-24 00:06:28 -0500

    Doing all the stuff and things

---
+++ 2022-23/playground.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.auto.util.HardAuto;
+import org.firstinspires.ftc.teamcode.auto.util.threads.ArmVertical;
+
+@Autonomous(name="Playground", group = "Auto")
+public class playground extends LinearOpMode {
+    HardAuto r = new HardAuto();
+    ArmVertical a;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+
+    }
