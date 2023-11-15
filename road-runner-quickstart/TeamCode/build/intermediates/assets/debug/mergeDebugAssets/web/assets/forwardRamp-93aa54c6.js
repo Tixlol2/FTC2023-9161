dbfe43fc73e0f95e214a577052d1b454ef1e594f
AsherChilli <asherchildress@gmail.com>
2023-11-15 16:53:56 -0600

    new hardware

---
+++ road-runner-quickstart/TeamCode/build/intermediates/assets/debug/mergeDebugAssets/web/assets/forwardRamp-93aa54c6.js
@@ -0,0 +1,1 @@
+import{i as a,b as n}from"./common-cced6619.js";window.addEventListener("DOMContentLoaded",function(){a("forward-ramp",n)});
---
+++ road-runner-quickstart/TeamCode/build/intermediates/assets/debug/mergeDebugAssets/web/assets/lateralRamp-369eb6af.js
@@ -0,0 +1,1 @@
+import{i as a,c as n}from"./common-cced6619.js";window.addEventListener("DOMContentLoaded",function(){a("lateral-ramp",n)});
