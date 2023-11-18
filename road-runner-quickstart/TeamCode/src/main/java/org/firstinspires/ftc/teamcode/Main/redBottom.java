83fbf0ec343fa9ff5d9582fe16ff841c0cf8d367
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-11-18 07:28:22 -0600

    Add files via upload

---
+++ road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/redBottom.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.Main;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@Autonomous(name = "redBottom", group = "Auto")
+public class redBottom extends LinearOpMode {
+
+
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+
+
+    public void runOpMode() throws InterruptedException {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        /*
+        double diameter = 0;
+
+        double circumference = (diameter * Math.PI);
+
+        double ticksPerInch = (28/circumference);
+
+        */
+
+        //i need to go 33 inches
+
+
+        //total distance = speed * time
+        //results in 500
+        //500 what???
+        run(.5,2000);
+
+
+    }
+
+
+    public void run(double power, int time){
+
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+
+        sleep(time);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+
+
+    }
+
+}
