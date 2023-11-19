7e9bd3d5e85dbc147c291c0fba8be881a9b145cd
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-11-18 20:13:58 -0600

    unsure of what i did last time so redoing this

---
+++ road-runner-quickstart/TeamCode/build/intermediates/assets/debug/mergeDebugAssets/web/assets/driveEncoderAngularRamp-83b42b5f.js
@@ -0,0 +1,1 @@
+import{i as n,a}from"./common-cced6619.js";window.addEventListener("DOMContentLoaded",function(){n("angular-ramp",a)});
---
+++ road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/HardAuto.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.auto.util;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class HardAuto extends Hardware {
+
+    //enums for directions, you can use switch and then case FORWARD : . . . etc.
+    public enum direction {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+}
+}
