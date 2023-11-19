23ce003fd6f49123b66011d637cc9b2049b1b4f6
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-11-18 21:06:48 -0600

    Add files via upload

---
+++ road-runner-quickstart/TeamCode/build/HardAuto.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Main;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class HardAuto extends hardware {
+
+    //enums for directions, you can use switch and then case FORWARD : . . . etc.
+    public enum direction {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+
+    public void moveIn(double inches, direction dir, double power) {
+        inches *= 62;
+        int in = (int) Math.round(inches);
+
+        switch (dir) {
+            case FORWARD:
+                frontLeft.setTargetPosition(in);
+                frontRight.setTargetPosition(-in);
+                backRight.setTargetPosition(in);
+                backLeft.setTargetPosition(-in);
+                break;
+            case BACKWARD:
+                frontLeft.setTargetPosition(-in);
+                frontRight.setTargetPosition(in);
+                backRight.setTargetPosition(-in);
+                backLeft.setTargetPosition(in);
+                break;
+            case LEFT:
+                frontLeft.setTargetPosition(in);
+                frontRight.setTargetPosition(in);
+                backRight.setTargetPosition(in);
+                backLeft.setTargetPosition(in);
+                break;
+            case RIGHT:
+                frontLeft.setTargetPosition(-in);
+                frontRight.setTargetPosition(-in);
+                backRight.setTargetPosition(-in);
+                backLeft.setTargetPosition(-in);
+                break;
+        }
+    }
+}
