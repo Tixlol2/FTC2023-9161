b34e991c4869c19a3711c6b6a0a9c53582232ebb
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2023-12-11 08:46:19 -0600

    Add files via upload

--- road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/HardAuto.java
+++ road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/HardAuto.java
@@ -1,16 +1,22 @@
 package org.firstinspires.ftc.teamcode.Main;
 
 
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class HardAuto extends hardware {
 
-    @Override
-    public void init_robot(OpMode opMode) {
-        super.init_robot(opMode);
-    }
+
+    double motorTickCount = 28;
+
+    double gearBoxMulti = 15;
+
+    double updatedMotorTickCount = motorTickCount * gearBoxMulti;
+
+    double radius = 48;
+    double circumference = 2 * (Math.PI) * radius;
+
+    double ticksPerInch = updatedMotorTickCount / circumference;
+
 
     //enums for directions, you can use switch and then case FORWARD : . . . etc.
     public enum direction {
@@ -20,16 +26,18 @@ public class HardAuto extends hardware {
         RIGHT
     }
 
-    public void moveIn(double inches, direction dir, double power) {
+    public void moveInEncoder(double inches, direction dir, double power) {
+
+
         inches *= 62;
         int in = (int) Math.round(inches);
 
         switch (dir) {
             case FORWARD:
-                frontLeft.setTargetPosition(in);
+                frontLeft.setTargetPosition(-in);
                 frontRight.setTargetPosition(-in);
                 backRight.setTargetPosition(in);
-                backLeft.setTargetPosition(-in);
+                backLeft.setTargetPosition(in);
                 break;
             case BACKWARD:
                 frontLeft.setTargetPosition(-in);
@@ -49,6 +57,101 @@ public class HardAuto extends hardware {
                 backRight.setTargetPosition(-in);
                 backLeft.setTargetPosition(-in);
                 break;
+
+
         }
+
+        for (DcMotor motor : Drive) {
+            motor.setPower(power);
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        }
+
+    }
+
+    public void moveIn(double time, direction dir, double power) {
+
+
+        switch (dir) {
+            case FORWARD:
+                frontLeft.setPower(power);
+                frontRight.setPower(-power);
+                backRight.setPower(-power);
+                backLeft.setPower(power);
+                break;
+            case BACKWARD:
+                frontLeft.setPower(-power);
+                frontRight.setPower(power);
+                backRight.setPower(power);
+                backLeft.setPower(-power);
+                break;
+            case LEFT:
+                frontLeft.setPower(power);
+                frontRight.setPower(power);
+                backRight.setPower(power);
+                backLeft.setPower(power);
+                break;
+            case RIGHT:
+                frontLeft.setPower(power);
+                frontRight.setPower(power);
+                backRight.setPower(power);
+                backLeft.setPower(power);
+                break;
+
+
+        }
+    }
+
+    public void goInches(double inches, direction dir, double power) {
+
+        int totalInches = (int) (inches * ticksPerInch);
+
+
+
+        switch (dir) {
+            case FORWARD:
+
+                for (DcMotor motor : Drive){
+
+                    motor.setTargetPosition(totalInches);
+                    if (motor == backLeft || motor == frontLeft){motor.setTargetPosition(-totalInches);}
+                    else {motor.setTargetPosition(totalInches);}
+                    motor.setPower(power);
+                    motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    opMode.telemetry.addData("Power FORWARD", Drive);
+                    opMode.telemetry.addData("Current", motor.getCurrentPosition());
+                    opMode.telemetry.addData("Target", motor.getTargetPosition());
+                }
+                while (frontLeft.isBusy()){
+                    opMode.telemetry.addData("FLM Current", frontLeft.getCurrentPosition());
+                    //Do Nothing
+                }
+                break;
+            case BACKWARD:
+                for (DcMotor motor : Drive){
+                    motor.setTargetPosition(totalInches);
+                    if (motor == backLeft || motor == frontLeft){motor.setTargetPosition(totalInches);}
+                    else {motor.setTargetPosition(-totalInches);}
+                    motor.setPower(power);
+                    motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    opMode.telemetry.addData("Power FORWARD", Drive);
+                    opMode.telemetry.addData("Current", motor.getCurrentPosition());
+                    opMode.telemetry.addData("Target", motor.getTargetPosition());
+                }
+                while (frontLeft.isBusy()){
+                    //Do Nothing
+                }
+                break;
+            case LEFT:
+
+                break;
+            case RIGHT:
+                break;
+
+
+        }
+        opMode.telemetry.update();
+
     }
 }
--- road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/hardware.java
+++ road-runner-quickstart/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/hardware.java
@@ -9,8 +9,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class hardware {
     OpMode opMode;
 
-    public DcMotor frontLeft, frontRight, backRight, backLeft, arm1, arm2;
+    public DcMotor frontLeft, frontRight, backRight, backLeft, clawLeft, clawRight, droneLauncher;
+
+    public DcMotor[] driveLeft, driveRight;
+
+
+
     public DcMotor[] Drive;
+    public Servo servo;
   
 
     public void init_robot(OpMode opMode) {
@@ -21,14 +27,37 @@ public class hardware {
 
     public void init_hardware() {
 
+        //Wheel Motor Def.
+
         frontLeft = opMode.hardwareMap.dcMotor.get("FLM");
         frontRight = opMode.hardwareMap.dcMotor.get("FRM");
         backLeft = opMode.hardwareMap.dcMotor.get("BLM");
         backRight = opMode.hardwareMap.dcMotor.get("BRM");
 
+        //Claw Motor Def.
+        clawLeft = opMode.hardwareMap.dcMotor.get("CL");
+        clawRight = opMode.hardwareMap.dcMotor.get("CR");
+        //clawAngle = opMode.hardwareMap.dcMotor.get("CLA");
+
+        //Servo Def.
+        servo = opMode.hardwareMap.servo.get("CLW");
+
+        //Drone Launch Def.
+        droneLauncher = opMode.hardwareMap.dcMotor.get("DLM");
+
+        droneLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        driveLeft = new DcMotor[]{frontLeft, backLeft};
+        driveRight = new DcMotor[]{frontRight, backRight};
+
         Drive = new DcMotor[]{frontLeft, backLeft, frontRight, backRight};
-        for (DcMotor motor: Drive){
+        for (DcMotor motor : Drive){
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            opMode.telemetry.addData("Motor Init for Drive", motor);
+            opMode.telemetry.update();
+
         }
 
         opMode.telemetry.update();
